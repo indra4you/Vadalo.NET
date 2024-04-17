2b7bbec92fa94fdb6e7759163a923e802e909c9e
Indrajeet Valera <indra4you@outlook.com>
2024-04-17 16:37:15 +0530

    Fixed unit tests

--- Vadalo.WebApi/UnitTests/Vadalo.WebApi.UnitTests/Controllers/HealthControllerTests.cs
+++ Vadalo.WebApi/UnitTests/Vadalo.WebApi.UnitTests/Controllers/HealthControllerTests.cs
@@ -2,7 +2,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Vadalo.Web.Api.Controllers;
@@ -15,15 +14,15 @@ public class HealthControllerTests
     public async Task HealthController_Get_HappyPath_ShouldHaveValidResponse(
     )
     {
-        var mockHealthCheckService = new Mock<HealthCheck.IHealthCheckService>(MockBehavior.Strict);
+        var mockHealthCheckService = new Mock<Vadalo.HealthCheck.IHealthCheckService>(MockBehavior.Strict);
         mockHealthCheckService
             .Setup(
                 expression => expression.CheckHealth(default)
             )
             .ReturnsAsync(
-                new HealthCheck.HealthReport(
+                new Vadalo.HealthCheck.HealthReport(
                     [],
-                    HealthCheck.HealthStatus.Healthy,
+                    Vadalo.HealthCheck.HealthStatus.Healthy,
                     TimeSpan.FromSeconds(0)
                 )
             )
@@ -45,16 +44,16 @@ public class HealthControllerTests
         Assert.IsNotNull(castedResponse);
         Assert.AreEqual(200, castedResponse.StatusCode);
         Assert.IsNotNull(castedResponse.Value);
-        Assert.IsInstanceOfType<ViewModel.ActionResponse<HealthCheck.HealthReport>>(castedResponse.Value);
+        Assert.IsInstanceOfType<ViewModel.ActionResponse<Vadalo.HealthCheck.HealthReport>>(castedResponse.Value);
 
-        var actionResponse = castedResponse.Value as ViewModel.ActionResponse<HealthCheck.HealthReport>;
+        var actionResponse = castedResponse.Value as ViewModel.ActionResponse<Vadalo.HealthCheck.HealthReport>;
         Assert.IsNotNull(actionResponse);
         Assert.AreEqual(ViewModel.ActionStatus.Successful, actionResponse.Status);
-        Assert.AreEqual($"Health Check Status is '{HealthCheck.HealthStatus.Healthy}'", actionResponse.Message);
+        Assert.AreEqual($"Health Check Status is '{Vadalo.HealthCheck.HealthStatus.Healthy}'", actionResponse.Message);
 
         Assert.IsNotNull(actionResponse.Data);
-        Assert.AreEqual(HealthCheck.HealthStatus.Healthy, actionResponse.Data.Status);
-        Assert.AreEqual(HealthCheck.HealthStatus.Healthy.ToString(), actionResponse.Data.StatusDescription);
+        Assert.AreEqual(Vadalo.HealthCheck.HealthStatus.Healthy, actionResponse.Data.Status);
+        Assert.AreEqual(Vadalo.HealthCheck.HealthStatus.Healthy.ToString(), actionResponse.Data.StatusDescription);
         Assert.AreEqual(TimeSpan.Zero, actionResponse.Data.TotalDuration);
         Assert.IsNotNull(actionResponse.Data.Entries);
         Assert.AreEqual(0, actionResponse.Data.Entries.Count);
